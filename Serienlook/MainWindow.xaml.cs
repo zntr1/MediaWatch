2f9cf85f23a7b1a0ce64066746544430267f164e
zntr1 <zesarex@gmail.com>
2017-09-03 23:08:02 +0200

    Add project files.

---
+++ Serienlook/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Serienlook{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow{
+        public MainWindow(){
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e){
+            Console.WriteLine("Button clicked!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e){
+            Console.WriteLine("Button Clicked");
+            Console.WriteLine("Buton22");
+        }
+    }
+}
